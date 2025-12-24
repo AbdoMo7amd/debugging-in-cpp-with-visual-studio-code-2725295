cdd716c80797782c9bb2bf1218d36b8909e9f0fb
AbdoMo7amd <abdom7mdcr@gmail.com>
2025-12-24 20:55:34 +0000

    Solve Errors

--- src/Ch04/04_05/CodeDemo.cpp
+++ src/Ch04/04_05/CodeDemo.cpp
@@ -7,36 +7,47 @@
 
 using std::vector;
 
-bool isPrime(int num){
-    if num < 2: 
-        return False
+bool isPrime(int num)
+{
+    if (num < 2)
+    {
+        return false;
+    }
+    if (num == 2)
+    {
+        return true; // 2 is prime
+    }
+    if ((num % 2) == 0)
+    {
+        return false; // Even numbers > 2 are NOT prime
+    }
 
-    if(num == 2){
-        return true;  // 2 is prime
+for (int i = 3; i * i <= num; i += 2)
+{ // Skip even numbers
+    if ((num % i) == 0)
+        return false;}
 
-    if((num % 2) = 0) 
-        return false;  // Even numbers > 2 are NOT prime
+        return true;
 
-    for(int i = 3, i * i <= num, i += 2)  // Skip even numbers
-        if((num % i) == 0) return false;
-
-    return true;
 }
 
-int sumOfPrimes(const vector<int>& numbers){
-    sum = 0;
-    for(int num in numbers)
-        if(isPrime(num))
+int sumOfPrimes(const vector<int> &numbers)
+{
+    int sum = 0;
+    for (int num : numbers){
+        if (isPrime(num))
             sum += num;
-    return sum;
+    return sum;}
 }
 
-int main(){
+int main()
+{
     vector<int> numbers = {70, 17, 12, 81, 5};
     int learnerResult = sumOfPrimes(numbers);
-    
+
     std::cout << "Your code returned: " << learnerResult << std::endl;
-    
-    std::cout << std::endl << std::endl;
+
+    std::cout << std::endl
+              << std::endl;
     return 0;
 }
